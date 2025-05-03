a2de4c827f7c8afe1f4cdd39072ef5602b22a16a
Sanika <sanika8356@gmail.com>
2025-05-03 13:48:15 +0530

    params congifured for the pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -28,7 +28,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-'''def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
@@ -43,7 +43,7 @@ logger.addHandler(file_handler)
         raise
     except Exception as e:
         logger.error('Unexpected error: %s', e)
-        raise'''
+        raise
 
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -86,9 +86,10 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        '''params = load_params(params_path='params.yaml')
-        test_size = params['data_ingestion']['test_size']'''
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+
+        #test_size = 0.2
         data_path = 'https://raw.githubusercontent.com/vikashishere/YT-MLOPS-Complete-ML-Pipeline/refs/heads/main/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -26,7 +26,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-'''def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
@@ -41,7 +41,7 @@ logger.addHandler(file_handler)
         raise
     except Exception as e:
         logger.error('Unexpected error: %s', e)
-        raise'''
+        raise
 
 def load_data(file_path: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -94,9 +94,9 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
 
 def main():
     try:
-        '''params = load_params(params_path='params.yaml')
-        max_features = params['feature_engineering']['max_features']'''
-        max_features = 50
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
+        #max_features = 50
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')

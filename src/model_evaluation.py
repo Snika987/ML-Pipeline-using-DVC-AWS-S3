b110e278e46263c225ca7ff2cc55bfa24acc78e9
Sanika <sanika8356@gmail.com>
2025-05-03 17:50:26 +0530

    with dvc

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,8 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
-'''import yaml
-from dvclive import Live'''
+import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -30,7 +30,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-'''def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
@@ -45,7 +45,7 @@ logger.addHandler(file_handler)
         raise
     except Exception as e:
         logger.error('Unexpected error: %s', e)
-        raise'''
+        raise
 
 def load_model(file_path: str):
     """Load the trained model from a file."""
@@ -112,7 +112,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-       #params = load_params(params_path='params.yaml')
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -120,25 +120,22 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
- 
+     
+ # Experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+            #live.log_image(str)
+            live.log_params(params)
+
+
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
         print(f"Error: {e}")
 
+
 if __name__ == '__main__':
     main()
-
-
-
-    ''' 
- # Experiment tracking using dvclive
-        with Live(save_dvc_exp=True) as live:
-            live.log_metric('accuracy', accuracy_score(y_test, y_test))
-            live.log_metric('precision', precision_score(y_test, y_test))
-            live.log_metric('recall', recall_score(y_test, y_test))
-
-            live.log_params(params)
-
-'''
